359a4dba4d28133c060afa94653f7cff59df7a17
RoyZiO <nziokinroy@gmail.com>
2023-03-24 07:35:28 +0300

    updates

---
+++ todo-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React  from  'react';
+
+function Header() {
+    return (
+      <div className="header-container">
+        <div className="header-text">
+          <h1 className="text-center">TRANQUILLO</h1>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Header;
