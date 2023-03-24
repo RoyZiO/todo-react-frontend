307ed398b321eec27abb796502a0615873758df3
RoyZiO <nziokinroy@gmail.com>
2023-03-24 07:47:30 +0300

    working on homepage

---
+++ todo-app/src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from "react";
+import { TodoListContext } from '../context/TodoListContext'
+
+function TodoForm () {
+    const [todo, setTodo] = useState('');
+    const [dueDate, setDueDate] = useState('');
+
+    const { handleAdd } = useContext(TodoListContext);
+
+    function handleSubmit (e) {
+        e.preventDefault();
+        if (todo.trim()) {
+            handleAdd({ task, dueDate });
+            setTodo('');
+            setDueDate('');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <label>
+          Todo:
+          <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+        </label>
+        <label>
+          Due Date:
+          <input type="date" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
+        </label>
+        <button type="submit">Add Todo</button>
+      </form>
+    );
+
+}
+export default TodoForm;
