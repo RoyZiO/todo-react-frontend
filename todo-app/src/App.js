2cf0a635ebeafe0a38b0cce653f25eb05244af4f
RoyZiO <nziokinroy@gmail.com>
2023-03-24 06:39:58 +0300

    created login page

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 
 function App() {
+
+  
   return (
     <LoginPage/>
   );
--- todo-app/src/pages/LoginPage.js
+++ todo-app/src/pages/LoginPage.js
@@ -1,70 +1,28 @@
+
 import React, { useState } from "react";
 
 function LoginPage(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    fetch("https://dummyjson.com/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ 
-        email: 'kminchelle@qq.com"', 
-        password: '0lelplR', })
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Invalid email or password");
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (data.success) {
-          window.location.href = "/HomePage";
-        } else {
-          throw new Error("Invalid email or password");
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        setError(error.message);
-      });
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
 
-  return (
-    <div>
-        <h1>TODO MANAGER</h1>
-      <h2>Login</h2>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleLogin}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
 }
 
 export default LoginPage;
